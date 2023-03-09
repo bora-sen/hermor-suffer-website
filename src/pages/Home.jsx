df0712dc21f4ebb4a71109abc541a274071ce0f3
bora-sen <bahattinborasen@gmail.com>
2023-03-09 16:04:25 +0300

    added website background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,17 @@ import Navbar from '../components/Navbar/Navbar';
 import PhotoCollage from '../components/PhotoCollage/PhotoCollage';
 import ProductList from '../components/ProductList/ProductList';
 import Footer from '../components/Footer/Footer';
+import Layout from '../components/Layout';
 
 function Home() {
   return (
     <>
+      <Layout>
         <Navbar />
         <PhotoCollage />
         <ProductList />
         <Footer />
+      </Layout>
     </>
   )
 }
